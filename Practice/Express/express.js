8949d65090bebc0c7c6b1cf74b487495118b084a
Piyushshind <piyushshinde.199@gmail.com>
2024-02-19 15:19:37 +0530

    post method

--- Practice/Express/express.js
+++ Practice/Express/express.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 7000;
+
+const allCourses = [
+    { id: 1, name: 'corse1' },
+    { id: 2, name: 'corse2' },
+    { id: 3, name: 'corse3' }
+];
 
 app.get('/', (req, res) => {
     res.send('hello World');
@@ -10,5 +17,20 @@ app.get('/', (req, res) => {
 app.get('/api/course/:year/:day', (req, res) => {
     res.send(req.params);
 });
+
+app.get('/api/courses/:id', (req, res) => {
+    const courss = allCourses.find(c => c.id === parseInt(req.params.id));
+    if (!courss) res.status(404).send('the givin course is not fount');
+    res.send(courss);
+})
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: allCourses.length + 1,
+        name: req.body.name
+    }
+    allCourses.push(course);
+    res.send(allCourses);
+})
 
 app.listen(port, () => console.log(`app is running on port ${port}...`));
