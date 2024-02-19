afe6bf7013e8ae6814dc33b42e97b8d83049c927
Piyushshind <piyushshinde.199@gmail.com>
2024-02-19 12:06:58 +0530

    EventEmitter

--- Practice/Event.js
+++ Practice/Event.js
@@ -2,9 +2,9 @@ const EventEmitter = require('events'); // it is class :- camaleCasing
 const event = new EventEmitter;
 
 // Register a listner 
-event.on('piyushCalledMessage', ()=>{
-    console.log( ' it is called ');
+event.on('piyushCalledMessage', (e)=>{
+    console.log( ' it is called ' , e);
 })
 
 // Raise an event
-event.emit('piyushCalledMessage');
+event.emit('piyushCalledMessage', {id:1,url:'http://piyush/get'});
